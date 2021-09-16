d5380226c07df5e2f59ddb374b6211f767fc872a
acizquierdo <acizquierdo@hiberus.com>
2021-09-16 18:30:08 +0200

    refactor 2.0 xd

--- MetalBake/MetalBake/Services/AccountService.cs
+++ MetalBake/MetalBake/Services/AccountService.cs
@@ -1,20 +1,12 @@
-﻿using MetalBake.Interfaces;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MetalBake.Services
 {
-    public class AccountService : IAccountable
+    public class AccountService 
     {
-        public bool CheckAmountAccount()
-        {
-            throw new NotImplementedException();
-        }
-
-        public decimal TotalAmountAccount()
-        {
-            throw new NotImplementedException();
-        }
+       
     }
 }
