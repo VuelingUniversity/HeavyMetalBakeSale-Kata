419a4f3a3a3a12d304161822c6cf1106861ce257
MNavarro1997 <mnavarro@gteam.es>
2021-09-19 16:53:26 +0200

    Main implementado

--- MetalBake/MetalBake/Program.cs
+++ MetalBake/MetalBake/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using MetalBake.Services;
+using System;
+using System.Text;
 
 namespace MetalBake
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            
+            InvetoryService ser = new InvetoryService();
+            PurchaseService ps = PurchaseService.GetInstance();
+            Console.WriteLine("Which products do you want? ");
+            string items = Console.ReadLine();
+            StringBuilder receipt = new StringBuilder();
+            string orderItems = ser.InventoryFilter(items);
+            decimal totalPrice = ps.OrderPrice(orderItems);
+            Console.WriteLine($"Total price: ${totalPrice}");
+            receipt.Append($"Buy: > {orderItems}, Total > ${totalPrice}");
+            Console.WriteLine("Money:");
+            decimal payed = 0;
+            Decimal.TryParse(Console.ReadLine(), out payed);
+            receipt.Append($", Payed > ${payed}");
+            if (payed >= totalPrice)
+            {
+                decimal exchange = ps.Purchase(totalPrice, payed);
+                if (exchange != 0)
+                {
+                    receipt.Append($", Cambio > ${exchange}");
+                }
+                receipt.Append(", Completed.");
+            }
+            else
+            {
+                receipt.Append(", It's not enough");
+            }
+            Console.WriteLine(receipt.ToString());
         }
     }
 }
