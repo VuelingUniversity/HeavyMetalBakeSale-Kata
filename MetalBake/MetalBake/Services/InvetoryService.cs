45e7376f47473f55ffd871c7c2ef82a1d5ee333d
MNavarro1997 <mnavarro@gteam.es>
2021-09-19 12:17:06 +0200

    creacion servicios

---
+++ MetalBake/MetalBake/Services/InvetoryService.cs
@@ -0,0 +1,75 @@
+﻿using MetalBake.Interfaces;
+using MetalBake.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetalBake.Services
+{
+    public class InvetoryService
+    {
+        private Dictionary<Type, int> stockList = SetItem();
+        private Brownie _brownie = new Brownie();
+        private Cake_Pop _cake_Pop = new Cake_Pop();
+        private Muffin _muffin = new Muffin();
+        private Water _water = new Water();
+
+        private static Dictionary<Type, int> SetItem()
+        {
+            Dictionary<Type, int> list = new Dictionary<Type, int>();
+            list.Add(typeof(Brownie), 25);
+            list.Add(typeof(Cake_Pop), 30);
+            list.Add(typeof(Muffin), 25);
+            list.Add(typeof(Water), 50);
+
+            return list;
+        }
+
+        public string InventoryFilter(string selectedItems)
+        {
+            char[] totalItems = selectedItems.Replace(",", string.Empty).ToCharArray();
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var item in totalItems)
+            {
+                if (item.Equals(_cake_Pop.ShortName))
+                {
+                    stringBuilder.Append(CheckStock(_cake_Pop));
+                }
+                else if (item.Equals(_water.ShortName))
+                {
+                    stringBuilder.Append(CheckStock(_water));
+                }
+                else if (item.Equals(_muffin.ShortName))
+                {
+                    stringBuilder.Append(CheckStock(_muffin));
+                }
+                else if (item.Equals(_brownie.ShortName))
+                {
+                    stringBuilder.Append(CheckStock(_brownie));
+                }
+                else
+                {
+                    Console.WriteLine($"El item {item} no existe");
+                }
+            }
+            string availableItems = stringBuilder.ToString();
+            availableItems = availableItems.Remove(availableItems.Length - 1);
+            return availableItems;
+        }
+
+        private string CheckStock(IProductable product)
+        {
+            if (stockList[product.GetType()] > 0)
+            {
+                stockList[product.GetType()]--;
+                return $"{product.ShortName},";
+            }
+            else
+            {
+                Console.WriteLine("No stock");
+                //se usa cuando no hay stock
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ MetalBake/MetalBake/Services/PurchaseService.cs
@@ -0,0 +1,69 @@
+﻿using MetalBake.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetalBake.Services
+{
+    public class PurchaseService
+    {
+        private Dictionary<Type, decimal> priceList = SetItem();
+
+        private static Dictionary<Type, decimal> SetItem()
+        {
+            Dictionary<Type, decimal> list = new Dictionary<Type, decimal>();
+            list.Add(typeof(Cake_Pop), 1.35m);
+            list.Add(typeof(Brownie), 0.65m);
+            list.Add(typeof(Water), 1.50m);
+            list.Add(typeof(Muffin), 1);
+            return list;
+        }
+
+        private Brownie _brownie = new Brownie();
+        private Cake_Pop _cakePop = new Cake_Pop();
+        private Muffin _muffin = new Muffin();
+        private Water _water = new Water();
+
+        private static PurchaseService _instance;
+
+        public decimal OrderPrice(string orderItems)
+        {
+            decimal total = 0;
+            char[] totalItems = orderItems.Replace(",", string.Empty).ToCharArray();
+            foreach (var item in totalItems)
+            {
+                if (item.Equals(_cakePop.ShortName))
+                {
+                    total += priceList[typeof(Cake_Pop)];
+                }
+                else if (item.Equals(_water.ShortName))
+                {
+                    total += priceList[typeof(Water)];
+                }
+                else if (item.Equals(_muffin.ShortName))
+                {
+                    total += priceList[typeof(Muffin)];
+                }
+                else if (item.Equals(_brownie.ShortName))
+                {
+                    total += priceList[typeof(Brownie)];
+                }
+            }
+            return total;
+        }
+
+        public decimal Purchase(decimal totalPrice, decimal payed)
+        {
+            return payed - totalPrice;
+        }
+
+        static public PurchaseService GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new PurchaseService();
+            }
+            return _instance;
+        }
+    }
+}
