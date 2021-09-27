4dce913702854026641f8998c795c0517b733b69
altitoo <alberto.vicente.bernad@gmail.com>
2021-09-27 09:26:54 +0200

    MetalBake

---
+++ MetalBake2/Models/Order.cs
@@ -0,0 +1,44 @@
+﻿using MetalBakeMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetalBakeMVC.Models
+{
+    public class Order
+    {
+        public List<StockData> productStock { get; set; }
+        public double Total { get; set; }
+
+        public Order(List<StockData> productList, double total)
+        {
+            this.productStock = productList;
+            Total = total;
+        }
+
+        public void AddProduct(StockData item)
+        {
+            if (productStock.Contains(item))
+            {
+                productStock.FirstOrDefault(product => product.Name == item.Name).Total += 1;
+                return;
+            }
+            productStock.Add(item);
+        }
+
+        public void ReduceProduct(StockData item)
+        {
+            if (productStock.Contains(item))
+            {
+                var product = productStock.FirstOrDefault(product => product.Name == item.Name);
+                if (product.Total > 1)
+                {
+                    product.Total -= 1;
+                    return;
+                }
+                productStock.Remove(item);
+            }
+        }
+    }
+}
