d0a351e11d2377ac6b199987f36fb6fef18fdd77
altitoo <alberto.vicente.bernad@gmail.com>
2021-09-27 09:40:24 +0200

    ShopingCart

---
+++ MetalBake2/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using MetalBakeMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetalBakeMVC.Models
+{
+    public class Order
+    {
+        public List<StockData> listOrder { get; set; }
+        public double Total { get; set; }
+    }
+}
---
+++ MetalBake2/ViewModels/OrderView.cs
@@ -0,0 +1,13 @@
+﻿using MetalBakeMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetalBakeMVC.ViewModels
+{
+    public class OrderView
+    {
+        public Order order { get; set; }
+    }
+}
